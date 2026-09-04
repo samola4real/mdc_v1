b4d2abbe299eb00a4b09a9df4b08fe88a2afd935
samola4real <samola4real@googlemail.com>
2026-09-04 12:19:29 +0300

    refactor: route public GET APIs through stable views

--- mdc-catalog/backend/apps/api/views/__init__.py
+++ mdc-catalog/backend/apps/api/views/__init__.py
@@ -1,6 +1,7 @@
 from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import Path
 
+from apps.api.views.get_views import catalog_filters, health
 from apps.api.views.post_views import service_discovery_search
 
 
@@ -9,10 +10,7 @@ _legacy_spec = spec_from_file_location("apps.api._legacy_views", _legacy_views_p
 _legacy_views = module_from_spec(_legacy_spec)
 _legacy_spec.loader.exec_module(_legacy_views)
 
-health = _legacy_views.health
-catalog_filters = _legacy_views.catalog_filters
 catalog_search = _legacy_views.catalog_search
 provider_publication = _legacy_views.provider_publication
 provider_detail = _legacy_views.provider_detail
 offering_detail = _legacy_views.offering_detail
-
